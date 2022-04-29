682bade7f6804e457126739498f2b8b02c10bb41
349225219 <101135430+349225219@users.noreply.github.com>
2022-04-29 13:49:54 -0400

    dfbc
    wer

--- elephant.java
+++ elephant.java
@@ -8,13 +8,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class elephant extends Actor
 {
-    /**
-     * Act - do whatever the elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    String name;
+    public animal(String name1)
+    {
+        name = name1;
+    }
+    
+    
     public void act()
     {
         // Add your action code here.
         move(-1);
+        
     }
 }
